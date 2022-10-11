eec54ae042a3377b9c737aa87b91221fcaee6406
misterlum <112566295+misterlum@users.noreply.github.com>
2022-10-11 09:43:13 -0400

    Add files via upload

---
+++ list_practice0.py
@@ -0,0 +1,72 @@
+#EXAMPLE: 0
+#0) Starting with the given list, append values to make the list equal to [1]
+
+list0 = []
+
+#IMPLEMENT HERE
+list0.append(1)
+
+print("Practice 0")
+print(list0 == [1])
+
+#PRACTICE: 1 through 7
+#For each practice, use append(), insert(), and remove() as necessary to make the
+#starting list match the resulting list.
+#1) Starting with the given list, make a list equal to [1, 2, 3]
+
+list1 = [1, 2]
+
+#IMPLEMENT HERE
+
+print("Practice 1")
+print(list1 == [1, 2, 3])
+
+#2) Starting with the given list, make a list equal to [1, 3]
+
+
+list2 = [1, 2, 3]
+
+#IMPLEMENT HERE
+
+print("Practice 2")
+print(list2 == [1, 3])
+
+#3) Starting list: [1, 2, 3, 2]
+#   Resulting list: [1, 3]
+
+list3 = [1, 2, 3, 2]
+
+#IMPLEMENT HERE
+
+print("Practice 3")
+print(list3 == [1, 3])
+
+#4) Starting list: ["Hello", "Goodbye"]
+#   Resulting list: ["Hello", "What?", "Goodbye"]
+
+list4 = ["Hello", "Goodbye"]
+
+#IMPLEMENT HERE
+
+print("Practice 4")
+print(list4 == ["Hello", "What?", "Goodbye"])
+
+#5) Starting list: [1.5, 2.0, 2.5]
+#   Resulting list: [1.0, 1.5, 2.0, 2.5]
+
+list5 = [1.5, 2.0, 2.5]
+
+#IMPLEMENT HERE
+
+print("Practice 5")
+print(list5 == [1.0, 1.5, 2.0, 2.5])
+
+#6) Starting list: []
+#   Resulting list: [1, 2, 3]
+
+list6 = []
+
+#IMPLEMENT HERE
+
+print("Practice 6")
+print(list6 == [1, 2, 3])
