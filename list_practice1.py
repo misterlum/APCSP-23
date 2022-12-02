e6cc4a52976da3f2a6a7b0a040cb5dfb9cdaef31
misterlum <112566295+misterlum@users.noreply.github.com>
2022-12-02 12:52:17 -0500

    Create list_practice1.py

---
+++ list_practice1.py
@@ -0,0 +1,160 @@
+'''
+Some more exercises to complete using lists in Python.
+
+Note that most of them use some sort of loop to iterate across a list,
+so most likely, you will use the for loop to do so
+'''
+
+'''
+Exercise 0 (example)
+
+Find the sum of a list
+'''
+print("Exercise 0")
+#list is defined below here
+list0 = [1, 3, 5, 7, 9, 11]
+
+print("Your total is")
+#implement your code below here
+total = 0
+
+for x in list0:
+    total = total + x
+
+#check your answer below here
+print(total)
+print("The total is supposed to be 36\n")
+
+
+
+'''
+Exercise 1
+
+Find the mean average of a list
+'''
+print("Exercise 1")
+#list is defined below here
+list1 = [2, 4, 6, 8, 10, 12]
+
+print("Your mean is")
+#implement your code below here
+
+
+
+#check your answer below here
+print("The mean is supposed to be 7.0\n")
+
+
+
+
+'''
+Exercise 2
+
+Print even numbers from a list
+'''
+print("Exercise 2")
+#list is defined below here
+list2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print("Your printout is")
+#implement your code below here
+
+
+
+#check your answer below here
+print("The printout should be \n2\n4\n6\n8\n\10\n")
+
+
+
+'''
+Exercise 3
+
+Print odd numbers from a list
+'''
+print("Exercise 3")
+#list is defined below here
+list3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print("Your printout is")
+#implement your code below here
+
+
+
+#check your answer below here
+print("The printout should be \n1\n3\n5\n7\n9\n")
+
+
+
+'''
+Exercise 4
+
+Count how many even numbers there are in a list
+'''
+print("Exercise 4")
+#list is defined below here
+list4 = [11, 22, 33, 44, 66, 77, 1010]
+
+print("Your count is")
+#implement your code below here
+
+
+
+#check your answer below here
+print("There are 4 even numbers\n")
+
+
+
+'''
+Exercise 5
+
+Count how many positive numbers there are in a list,
+and count how many negative numbers there are.
+'''
+print("Exercise 5")
+#list is defined below here
+list5 = [2, 85, -49, 91, 4, -96, 77]
+
+print("Your count is")
+#implement your code below here
+
+
+
+#check your answer below here
+print("There are 5 positive numbers")
+print("There are 2 negative numbers\n")
+
+
+
+'''
+Exercise 6
+
+Print all words starting with the letter 'a'
+'''
+print("Exercise 6")
+#list is defined below here
+list6 = ['apple', 'banana', 'avocado', 'april', 'zebra', 'motor']
+
+print("Your a words")
+#implement your code below here
+
+
+
+#check your answer below
+print("The a words are \napple\navocado\napril\n")
+
+'''
+Exercise 7
+
+Print all words starting with a vowel
+'''
+print("Exercise 7")
+#list is defined below here
+list7 = ['apple', 'egg', 'milk', 'bread', 'ice cream']
+
+print("Your words that start with a vowel are")
+#implement your code below here
+
+
+
+#check your answer below here
+print("The words that start with a vowel are \napple\negg\nice cream\n")
